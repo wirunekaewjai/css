5182f13e3234aa6560313b74e8ef41c5796d5929
Wirune Kaewjai <wirune.kae@hotmail.com>
2020-12-25 11:02:27 +0700

    v0.5.17 - ลดการใช้ temp file ตอน build ด้วยการใช้ externals แทน

--- src/bin/utils/evaluator/get-code.ts
+++ src/bin/utils/evaluator/get-code.ts
@@ -1,48 +1,76 @@
 import { buildSync } from 'esbuild';
-import { nanoid } from 'nanoid';
-import { readFileSync, writeFileSync, existsSync, unlinkSync } from 'fs';
-import { parse, join } from 'path';
+// import { nanoid } from 'nanoid';
+// import { readFileSync, writeFileSync, existsSync, unlinkSync } from 'fs';
+// import { parse, join } from 'path';
 
-function createTempName (file: string)
+const lowerCaseIgnoreds = [
+  '*.jpeg',
+  '*.jpg',
+  '*.png',
+  '*.gif',
+  '*.webp',
+  
+  '*.css',
+  '*.scss',
+  '*.sass',
+  '*.less',
+
+  '*.pdf',
+  '*.json',
+  '*.gql',
+];
+
+const ignoreds = [...lowerCaseIgnoreds, ...toUpperCase(lowerCaseIgnoreds)];
+
+function toUpperCase (arr: string[])
 {
-  const id = nanoid();
-  const p = parse(file);
-  const dir = p.dir;
-  const dst = join(dir, p.name + '.' + id + '.wkcsstmp' + p.ext);
-
-  return dst;
+  return arr.map(e => e.toUpperCase());
 }
 
-function createTempFile (src: string, dst: string)
-{
-  const srcCode = readFileSync(src).toString('utf8');
-  const dstCode = srcCode.replace(/import\s*('|")[^'"]+('|")/g, t => '// ' + t);
+// function createTempName (file: string)
+// {
+//   const id = nanoid();
+//   const p = parse(file);
+//   const dir = p.dir;
+//   const dst = join(dir, p.name + '.' + id + '.wkcsstmp' + p.ext);
 
-  writeFileSync(dst, dstCode);
-}
+//   return dst;
+// }
+
+// function createTempFile (src: string, dst: string)
+// {
+//   const srcCode = readFileSync(src).toString('utf8');
+//   const dstCode = srcCode.replace(/import\s*('|")[^'"]+('|")/g, t => '// ' + t);
+
+//   writeFileSync(dst, dstCode);
+// }
 
 export default function evaluate (file: string, externals?: string[])
 {
-  const dst = createTempName(file);
+  // const dst = createTempName(file);
 
   try
   {
-    createTempFile(file, dst);
+    // createTempFile(file, dst);
 
     const result = buildSync({
       bundle: true,
       platform: 'node',
-      external: externals,
-      entryPoints: [dst],
+      external: [
+        ...(externals ?? []),
+        ...ignoreds,
+      ],
+      // entryPoints: [dst],
+      entryPoints: [file],
       write: false,
     });
   
     const code = result?.outputFiles?.[0]?.text ?? '';
 
-    if (existsSync(dst))
-    {
-      unlinkSync(dst);
-    }
+    // if (existsSync(dst))
+    // {
+    //   unlinkSync(dst);
+    // }
 
     return code;
   }
@@ -50,9 +78,9 @@ export default function evaluate (file: string, externals?: string[])
   {
     console.error(err);
 
-    if (existsSync(dst))
-    {
-      unlinkSync(dst);
-    }
+    // if (existsSync(dst))
+    // {
+    //   unlinkSync(dst);
+    // }
   }
 }
